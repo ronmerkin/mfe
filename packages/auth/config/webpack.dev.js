1cc5df6a840d7fbc97ccdceb864fc414ed512318
rmerkin <ron_merkin@intuit.com>
2022-05-19 08:54:26 +0300

    added auth sub app

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -3,22 +3,27 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
-const prodConfig = {
-  mode: "production",
+const devConfig = {
+  mode: "development",
   output: {
-    filename: "[name].[contenthash].js",
-    publicPath: '/marketing/latest/'
+    publicPath: "http://localhost:8082/",
+  },
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      index: "index.html",
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
+      name: "auth",
       filename: "remoteEntry.js",
       exposes: {
-        "./MarketingApp": "./src/bootstrap",
+        "./AuthApp": "./src/bootstrap",
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
 
-module.exports = merge(commonConfig, prodConfig);
+module.exports = merge(commonConfig, devConfig);
